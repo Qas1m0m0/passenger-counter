ab2ee44a7ff8a180845f5985b912734cd5ccf097
Qas1m0m0 <Qasim.17@hotmail.com>
2023-08-24 14:19:01 +0100

    added some more styling, counter is working with reset

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // .innerText = 5
 
 let countEl = document.getElementById('count-el');
-
+let saveEl = document.getElementById('save-el');
 let count = 0
 
 function increment() {
     count = count + 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
     console.log(countEl.innerText)
+}
+
+function save() {
+     let entryCount = count + ' - ';
+     saveEl.textContent += entryCount;
+     countEl.textContent = 0;
+     count = 0;
+     console.log(count)
 }
